7629d4477c087975b4a3d3d9c00dedb863c9e9e0
Max Melamed <maxm897@Gmail.com>
2018-07-16 00:14:19 -0400

    more playByPlay

--- playByPlay.py
+++ playByPlay.py
@@ -1,7 +1,9 @@
 from astropy.table import Table, Column
-#import Setup
+from setup import *
+
 
 def main():
+    stats = {}
     file = open("Basketball_Analytics/NBA Hackathon - Play by Play Data Sample (50 Games).txt", "r")
     arr = []
     nested_arr=[]
@@ -12,12 +14,27 @@ def main():
         parsed=parse2(arr[k])
         print(parsed[2])
         if parsed[2] in ['1','3','8']:
-            
+
             nested_arr.append(parsed);
-    
-    print(groupByGame(nested_arr)["021fd159b55773fba8157e2090fe0fe2"])
-    
-    
+
+    dict=groupByGame(nested_arr)
+
+    for game in dict:
+        quarter_dict=groupByQuarter(dict[game])
+
+        dict[game]=quarter_dict
+
+
+    for game in dict:
+        if game not in stats:
+            stats[game]={}
+        for quarter in dict[game]:
+            processQ(dict[game][quarter])
+
+
+
+
+
 def parse2(line):
     res=[]
     copy = line +"\t"
@@ -26,39 +43,9 @@ def parse2(line):
         res.append(copy[:ind])
         copy=copy[ind+1:]
     return res
-    
-def parse(line):
-    """takes in a line from an array and returns an array of the elements inside in the following order: Game_id	Event_Num
-    Event_Msg_Type	Period	WC_Time	PC_Time	Action_Type	Option1	Option2	Option3	Team_id	Person1	Person2	Team_id_type"""
-    print(line)
-    copy=line+" "
-    res=[]
-    while copy!='':
-        k=before_space(copy)
-        if k!='':
-            res.append(k)
-        copy=copy[copy.index(" ")+1:]
-        remove_leading_space(copy)
-    print(res)
-
-    return res
 
 
-def before_space(s):
-    """Returns: Substring of s; up to, but not including, the first space
 
-    Parameter s: the string to slice
-    Precondition: s has at least one space in it"""
-    end=s.index(" ")
-    
-    return s[:end]
-
-def remove_leading_space(s):
-    """removes leading spaces from s"""
-    for i in range(len(s)):
-        if s[i].isalnum():
-            return s[i:]
-        
 def groupByGame(nested_arr):
     gamesDict={}
     for arr in nested_arr:
@@ -67,7 +54,34 @@ def groupByGame(nested_arr):
         else:
             gamesDict[arr[0]].append(arr)
     return gamesDict
-            
-    
+
+def groupByQuarter(nested_arr):
+    quartersDict={}
+    for arr in nested_arr:
+        if arr[3] not in quartersDict:
+            quartersDict[arr[3]]=[arr]
+        else:
+            quartersDict[arr[3]].append(arr)
+
+        for quarter in quartersDict:
+            sorted=sortEvents(quartersDict[quarter])
+            quartersDict[quarter]=sorted
+
+        return quartersDict
+
+def processQ(quarter):
+    for event in quarter:
+        if event[2]=='1':
+            processMadeShot(event)
+        elif event[2]=='3':
+            processFT(event)
+        elif event[2]=='8':
+            processSub(event)
+        else:
+            print("something has gone horribly wrong")
+
+def processMadeShot(event):
+
+
 if __name__=="__main__":
     main()
