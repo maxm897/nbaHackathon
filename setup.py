2ff755bcb5a23b9d325fa07699e4c59d2f7db3de
Karson Daecher <karsondaecher@Karsons-MacBook-Air.local>
2018-07-15 20:52:31 -0700

    added sorting algorithm to setup.py

--- setup.py
+++ setup.py
@@ -36,3 +36,24 @@ def addMinus2(dict, gameID, playerID):
 
 def addMinus3(dict, gameID, playerID):
     dict[gameID][playerID]["minus"] += 3
+
+def sortEvents(gameArr):
+"""Given: an array containing all the events belonging to a given quarter where
+each event is, itself, an array (that array is a line of the events.txt file).
+Returns: the same array, now reorded so that each event array is sorted by game
+clock time"""
+    result = [];
+    result.push(gameArr[0])
+    for (i = 1; i < gameArr.length; i++){
+        inserted = false
+        for(j = 0; j < result.length; j++) {
+            if(gameArr[i][6] <= result[j][6] && !inserted) {
+                result.insert(j, gameArr[i][6])
+                inserted = true
+            }
+        }
+        if (!inserted) {
+        result.append(gameArr[i][6])
+        }
+    }
+    return result;
